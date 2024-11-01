0c6938a575ce7f6d71995225d15bb5a0f826ca6c
Mica <166014977+callenmica@users.noreply.github.com>
2024-11-02 01:46:45 +0800

    Update page.tsx

--- src/app/stresswall/page.tsx
+++ src/app/stresswall/page.tsx
@@ -6,6 +6,8 @@ interface Submission {
   id: number
   stress: string
   name: string
+  prayers: number      // Added to track number of prayers
+  hasReacted?: boolean // Added to track if user has reacted
 }
 
 export const dynamic = 'force-dynamic'
